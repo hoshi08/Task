ad19942e1602e9b5860c07b2821f0816994e2208
hoshi <h08kou01@gmail.com>
2016-12-19 17:42:56 +0900

    try、catch修正 インデント修正

--- src/jp/co/iccom/hoshi_kouji/calculate_sales/calculate.java
+++ src/jp/co/iccom/hoshi_kouji/calculate_sales/calculate.java
@@ -17,38 +17,43 @@ import java.util.Map.Entry;
 public class calculate {
 
 	public static void main(String args[]) {
-		//入力処理
-		File file = new File(args[0], "blanch.lst");
-		File file1 = new File(args[0], "commodity.lst");
 
+		// 支店定義ファイル
+		File blanchfileIn = new File(args[0], "blanch.lst");
+
+		// 商品定義ファイル
+		File commodityfileIn = new File(args[0], "commodity.lst");
+
+		// ディレクトリ格納ファイル名
 		File dir = new File(args[0]);
-		File files[] = dir.listFiles();
+		File Namefiles[] = dir.listFiles();
 
 		String str;
 
-		//出力処理（支店定義）
-		File file2 = new File(args[0], "branch.out");
-		//出力処理（商品定義）
-		File file3 = new File(args[0], "commodity.out");
+		// 出力処理（支店定義）
+		File blanchfileOut = new File(args[0], "branch.out");
 
-
+		// 出力処理（商品定義）
+		File commodityfileOut = new File(args[0], "commodity.out");
 
 		// 処理1
 		// existsはフォルダの存在があるかないかを確認するメソッド
-		if (!file.exists()) {
+		if (!blanchfileIn.exists()) {
 
 			System.out.println("支店定義ファイルは存在しません");
 		}
 
 		// Hashmapを宣言する
-		HashMap<String, String> branchname = new HashMap<String, String>();
+		HashMap<String, String> branchNameMap = new HashMap<String, String>();
 
 		// 集計3-2で使用する変数
-		HashMap<String, Long> branchcodemap = new HashMap<String, Long>();
+		HashMap<String, Long> branchCodeMap = new HashMap<String, Long>();
+
+		BufferedReader br = null;
 
 		try {
-			FileReader fr = new FileReader(file);
-			BufferedReader br = new BufferedReader(fr);
+			FileReader fr = new FileReader(blanchfileIn);
+			br = new BufferedReader(fr);
 			while ((str = br.readLine()) != null) {
 				String array[] = str.split(",");
 
@@ -57,149 +62,160 @@ public class calculate {
 					System.out.println("支店定義ファイルのファーマットが不正です");
 				}
 				// 代入
-				branchname.put(array[0], array[1]);
-				branchcodemap.put(array[0], 0L);
+				branchNameMap.put(array[0], array[1]);
+				branchCodeMap.put(array[0], 0L);
 
 			}
-			br.close();
+
 		} catch (IOException e) {
-			System.out.println(e);
+			System.out.println("予期せぬエラーが発生しました");
 
-		}
-
-		// System.out.println(branchname.entrySet());
-		// System.out.println(branchcodemap.entrySet());
-
-
-
-
-		// 処理2
-		if (!file1.exists()) {
-
-			System.out.println("商品定義ファイルは存在しません");
-
-		}
-
-		HashMap<String, String> commodity = new HashMap<String, String>();
-
-		HashMap<String, Long> commoditycodemap = new HashMap<String, Long>();
-
-		try {
-			FileReader fr = new FileReader(file1);
-			BufferedReader br = new BufferedReader(fr);
-			while ((str = br.readLine()) != null) {
-				String array[] = str.split(",");
-
-				if (!array[0].matches("^[0-9A-Z]{8}$") || array.length != 2) {
-
-					System.out.println("商品定義ファイルのファーマットが不正です");
-					return;
+		} finally {
+			try {
+				if (br != null) {
+					br.close();
 				}
+			} catch (IOException e) {
+				System.out.println("予期せぬエラーが発生しました");
+			}
 
-				commodity.put(array[0], array[1]);
-				commoditycodemap.put(array[0], 0L);
+			// 処理2
+			if (!commodityfileIn.exists()) {
+
+				System.out.println("商品定義ファイルは存在しません");
 
 			}
 
-			br.close();
-		} catch (IOException a) {
-			System.out.println(a);
+			HashMap<String, String> commodityNameMap = new HashMap<String, String>();
 
-		}
-
-		// 処理3
-		ArrayList<File> foo = new ArrayList<File>();
-
-		// ArrayListに1つずつ格納処理
-		for (File f : files) {
-			// rcdファイルかどうかの判定
-			if (f.getName().matches("^\\d{8}.rcd$")) {
-				foo.add(f);
-				// System.out.println(f);
-			}
-
-			// 連番チェック
-			// for (File f : foo) {
-			// System.out.println(f);
-
-			// }
-
-		}
-
-		// 正規表現で取り出したものを取り出す処理
-		for (File f : foo) {
-			System.out.println(f);
+			HashMap<String, Long> commodityCodeMap = new HashMap<String, Long>();
 
 			try {
-				FileReader fr = new FileReader(f);
-				BufferedReader br = new BufferedReader(fr);
-
-				ArrayList<String> codelist = new ArrayList<String>();
-
+				FileReader fr = new FileReader(commodityfileIn);
+				br = new BufferedReader(fr);
 				while ((str = br.readLine()) != null) {
-					codelist.add(str);
+					String array[] = str.split(",");
+
+					if (!array[0].matches("^[0-9A-Z]{8}$") || array.length != 2) {
+
+						System.out.println("商品定義ファイルのファーマットが不正です");
+						return;
+					}
+
+					commodityNameMap.put(array[0], array[1]);
+					commodityCodeMap.put(array[0], 0L);
+
 				}
 
-				long rcdValue = Long.parseLong(codelist.get(2));
+			} catch (IOException a) {
+				System.out.println("予期せぬエラーが発生しました");
 
-				long branchVal = branchcodemap.get(codelist.get(0)) + rcdValue;
+			} finally {
 
-				long commodityVal = commoditycodemap.get(codelist.get(1)) + rcdValue;
+				try {
+					if (br != null) {
+						br.close();
+					}
+				} catch (IOException e) {
+					System.out.println("予期せぬエラーが発生しました");
+				}
 
-				branchcodemap.put(codelist.get(0), branchVal);
-				commoditycodemap.put(codelist.get(1), commodityVal);
+				// 処理3
+				ArrayList<File> foo = new ArrayList<File>();
 
-				// System.out.println(branchVal);
-				// System.out.println(commodityVal);
-				// System.out.println(branchcodemap.entrySet());
-				// System.out.println(commoditycodemap.entrySet());
+				// ArrayListに1つずつ格納処理
+				for (File f : Namefiles) {
+					// rcdファイルかどうかの判定
+					if (f.getName().matches("^\\d{8}.rcd$")) {
+						foo.add(f);
 
-			} catch (IOException e) {
-				System.out.println(e);
-			}
-		}
+					}
 
+					// 連番チェック
+					// for (File f : foo) {
+					// System.out.println(f);
 
-		// 降順に並べ替え
-		// List 生成 (ソート用)
-		//支店定義出力
-		List<Map.Entry<String, Long>> entries = new ArrayList<Map.Entry<String, Long>>(branchcodemap.entrySet());
-		Collections.sort(entries, new Comparator<Map.Entry<String, Long>>() {
+					// }
 
-			@Override
-			public int compare(Entry<String, Long> entry1, Entry<String, Long> entry2) {
+				}
 
-				// 自動生成されたメソッド・スタブ
-				return ((Long) entry2.getValue()).compareTo((Long) entry1.getValue());
+				// 正規表現で取り出したものを取り出す処理
+				for (File f : foo) {
+					// System.out.println(f);
 
-			}
-		});
-		// 内容を表示
+					try {
+						FileReader fr = new FileReader(f);
+						br = new BufferedReader(fr);
 
-		for (Entry<String, Long> s : entries) {
-			System.out.println(s.getKey() + ","  + branchname.get(s.getKey()) +  "," + s.getValue());
-		}
+						ArrayList<String> codelist = new ArrayList<String>();
 
-		try {
-			FileWriter fi = new FileWriter(file2);
-			BufferedWriter bw = new BufferedWriter(fi);
+						while ((str = br.readLine()) != null) {
+							codelist.add(str);
+						}
 
-			String separator = System.getProperty("line.separator");
+						long rcdValue = Long.parseLong(codelist.get(2));
 
-			for (Entry<String, Long> s : entries) {
-				bw.write((s.getKey() + ","  + branchname.get(s.getKey()) +  "," + s.getValue()) + separator);
-			}
-			bw.close();
+						long branchVal = branchCodeMap.get(codelist.get(0)) + rcdValue;
 
+						long commodityVal = commodityCodeMap.get(codelist.get(1)) + rcdValue;
 
-		} catch (IOException e) {
-			System.out.println(e);
-		}
+						branchCodeMap.put(codelist.get(0), branchVal);
+						commodityCodeMap.put(codelist.get(1), commodityVal);
 
+					} catch (IOException e) {
+						System.out.println("予期せぬエラーが発生しました");
+					} finally {
+						try {
+							if (br != null) {
+								br.close();
+							}
+						} catch (IOException e) {
+							System.out.println("予期せぬエラーが発生しました");
+						}
 
-		//商品定義出力
-				List<Map.Entry<String, Long>> entries1 = new ArrayList<Map.Entry<String, Long>>(commoditycodemap.entrySet());
-				Collections.sort(entries1, new Comparator<Map.Entry<String, Long>>() {
+					}
+
+				}
+
+				// 降順に並べ替え List 生成 (ソート用)
+				// 支店定義出力
+				List<Map.Entry<String, Long>> branchEntries = new ArrayList<Map.Entry<String, Long>>(
+						branchCodeMap.entrySet());
+				Collections.sort(branchEntries, new Comparator<Map.Entry<String, Long>>() {
+
+					@Override
+					public int compare(Entry<String, Long> entry1, Entry<String, Long> entry2) {
+
+						// 自動生成されたメソッド・スタブ
+						return ((Long) entry2.getValue()).compareTo((Long) entry1.getValue());
+
+					}
+				});
+
+				// 内容を表示
+				for (Entry<String, Long> s : branchEntries) {
+					System.out.println(s.getKey() + "," + branchNameMap.get(s.getKey()) + "," + s.getValue());
+				}
+
+				try {
+					FileWriter fi = new FileWriter(blanchfileOut);
+					BufferedWriter bw = new BufferedWriter(fi);
+
+					String separator = System.getProperty("line.separator");
+
+					for (Entry<String, Long> s : branchEntries) {
+						bw.write((s.getKey() + "," + branchNameMap.get(s.getKey()) + "," + s.getValue()) + separator);
+					}
+					bw.close();
+
+				} catch (IOException e) {
+					System.out.println("予期せぬエラーが発生しました");
+				}
+
+				// 商品定義出力
+				List<Map.Entry<String, Long>> commodityEntries = new ArrayList<Map.Entry<String, Long>>(
+						commodityCodeMap.entrySet());
+				Collections.sort(commodityEntries, new Comparator<Map.Entry<String, Long>>() {
 
 					@Override
 					public int compare(Entry<String, Long> entry1, Entry<String, Long> entry2) {
@@ -211,27 +227,27 @@ public class calculate {
 				});
 				// 内容を表示
 
-				for (Entry<String, Long> s : entries1) {
-					System.out.println(s.getKey() + ","  + commodity.get(s.getKey()) +  "," + s.getValue());
+				for (Entry<String, Long> s : commodityEntries) {
+					System.out.println(s.getKey() + "," + commodityNameMap.get(s.getKey()) + "," + s.getValue());
 				}
 
 				try {
-					FileWriter fi = new FileWriter(file3);
+					FileWriter fi = new FileWriter(commodityfileOut);
 					BufferedWriter bw = new BufferedWriter(fi);
 
 					String separator = System.getProperty("line.separator");
 
-					for (Entry<String, Long> s : entries1) {
-						bw.write((s.getKey() + ","  + commodity.get(s.getKey()) +  "," + s.getValue()) + separator);
+					for (Entry<String, Long> s : commodityEntries) {
+						bw.write(
+								(s.getKey() + "," + commodityNameMap.get(s.getKey()) + "," + s.getValue()) + separator);
 					}
 					bw.close();
 
-
 				} catch (IOException e) {
-					System.out.println(e);
+					System.out.println("予期せぬエラーが発生しました");
 				}
 
+			}
+		}
 	}
 }
-
-
